bd48b943aec577e4d0882001b860966ac9647d1a
Student <student@MacBook-Pro-von-Student.local>
2023-04-11 12:22:58 +0200

    routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 function App() {
-
-  return ()}
-export default App
+  return (
+    <>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
+export default App;
