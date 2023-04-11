5583d0273529f3e5cc0e5c1ade74f9a9644904d5
Student <student@MacBook-Pro-von-Student.local>
2023-04-11 12:00:51 +0200

    better

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
