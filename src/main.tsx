4803e7f6fd2d80360aa2d848f4d9f9ebec7a87de
Student <student@MacBook-Pro-von-Student.local>
2023-04-11 11:37:50 +0200

    better

--- src/main.tsx
+++ src/main.tsx
@@ -9,3 +9,9 @@ import {
 } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
